a8dbdff25cab433445b283d2037ef80bb0f3c55b
YunshiuanOAO <96372145+YunshiuanOAO@users.noreply.github.com>
2022-04-24 21:37:26 +0800

    Delete 題庫 directory

--- 題庫/基本輸出練習/基本輸出練習.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-	cout << "Name" << "\t" << "No." << "\t" <<  "Score" << endl
-         << "John" << "\t" << "20"  << "\t" <<  "40" << endl
-         << "Andy" << "\t" << "21"  << "\t" <<  "100" << endl;
-}
--- 題庫/簡單輸出文字/簡單輸出文字.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
- 
-int main() {	
-	printf("Hello World!\nThis is my first program.");
-	return 0;
-}
