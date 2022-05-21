fdfb111accc444f574ac3c398a6abf576eac11d6
YunshiuanOAO <96372145+YunshiuanOAO@users.noreply.github.com>
2022-05-21 17:54:30 +0800

    Update C.cpp

--- Google Code Jam/2021/C.cpp
+++ Google Code Jam/2021/C.cpp
@@ -1,11 +1,11 @@
-// Problem: Task
-// Contest: Google Coding Competitions - Qualification Round 2021 - Code Jam 2021
-// URL: https://codingcompetitions.withgoogle.com/codejam/round/000000000043580a/00000000006d12d7#problem
-// Memory Limit: 1024 MB
-// Time Limit: 10000 ms
+// Problem: Apple Division
+// Contest: CSES - CSES Problem Set
+// URL: https://cses.fi/problemset/task/1623
+// Memory Limit: 512 MB
+// Time Limit: 1000 ms
 // 
 // Powered by CP Editor (https://cpeditor.org)
-
+ 
 #include<iostream>
 #include<iomanip>
 #include<math.h>
@@ -22,41 +22,40 @@
 #include<string.h>
 #define int long long int
 #define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define T int _T;cin >> _T;while(_T--)cout << "Case#" << _T << ":";
+#define T int _T;cin >> _T;while(_T--)
 using namespace std;
-const int SIZE = 102;
-
-void solve(){
-	int n,c;
-	int a[SIZE]={},b[SIZE]={};
-	cin >> n >> c;
-	if(c<n-1 || c>n*(n+1)/2-1)cout << "IMPOSSIBLE";
-	else{
-		c-=n-1; 
-		for(int i=1;i<=n;i++)a[i]=i;
-		for(int i=1;i<n;i++){
-			if(c>=n-i){
-				c-=n-i;
-				reverse(a+i,a+n+1);
-			}
-		}
-		for(int i=1;i<=n;i++)b[a[i]]=i;
-		for(int i=1;i<=n;i++){
-			cout << b[i] << " ";
-		}
-	}
+const int SIZE=1<<21;
+int a[SIZE];
+ 
+int get_bit(int x,int y){
+    return (x>>y)&1;
 }
-
-
+ 
+void solve(){
+    int n,sum=0;
+    int ans=1LL<<60;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+        sum+=a[i];
+    }
+    int now=0;
+    for(int i=1;i<(1<<n);i++){
+        for(int j=0;j<n;j++){
+            if(get_bit(i,j)){
+                now+=a[j];
+                break;
+            }else{
+            	now-=a[j];
+            }
+        }
+        ans=min(ans,abs(sum-2*now));
+    }
+    cout << ans << '\n';
+}
+ 
+ 
 signed main(){
     IOS
-    int t;
-    cin >> t;
-    for(int i=0;i<t;i++){
-    	cout << "Case #" << i+1 << ": ";
-    	solve();
-    	if(i!=t-1)cout << '\n';
-    }
-    
+    solve();
 }
-
